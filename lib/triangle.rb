4c0a6500cee25891365313022a9ada85e706da49
Seve Sheldon <sevesheldon@gmail.com>
2017-04-05 13:44:51 -0700

    add all passing specs.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,4 +15,14 @@ class Triangle
     @side1.+(@side2).>(@side3) && @side2.+(@side3).>(@side1) && @side3.+(@side1).>(@side2)
   end
 
+  define_method(:isosceles?) do
+    @side1.eql?(@side2) || @side2.eql?(@side3) || @side3.eql?(@side1)
+    is_a_triangle?()
+  end
+
+  define_method(:scalene?) do
+    @side1 != @side2 && @side2 != @side3 && @side3 != @side1
+    is_a_triangle?()
+  end
+
 end
--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -18,6 +18,7 @@ describe(Triangle) do
   end
 
   describe("#is_a_triangle?") do
+
     it("returns false if the the sum of the lengths of any two sides is less than or equal to the length of the third side") do
       test_triangle = Triangle.new(2, 2, 8)
       expect(test_triangle.is_a_triangle?()).to(eq(false))
@@ -28,6 +29,33 @@ describe(Triangle) do
       expect(test_triangle.is_a_triangle?()).to(eq(true))
     end
 
+  end
+
+  describe("#isosceles?") do
+
+    it("returns false if exactly two sides are equal but #is_a_triangle? returns false") do
+      test_triangle = Triangle.new(2, 2, 8)
+      expect(test_triangle.isosceles?()).to(eq(false))
+    end
+
+    it("returns true if exactly two sides are equal and #is_a_triangle? returns true") do
+      test_triangle = Triangle.new(15, 15, 20)
+      expect(test_triangle.isosceles?()).to(eq(true))
+    end
+
+  end
+
+  describe("#scalene?") do
+
+    it("returns false if no sides are equal but #is_a_triangle? returns false") do
+      test_triangle = Triangle.new(1, 4, 30)
+      expect(test_triangle.scalene?()).to(eq(false))
+    end
+
+    it("returns true is no sides are equal and #is_a_triangle? returns true") do
+      test_triangle = Triangle.new(5, 6, 7)
+      expect(test_triangle.scalene?()).to(eq(true))
+    end
   end
 
 end
